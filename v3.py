1ba8ae090cad3dafcf71b33c7f3ff0101cacd9cd
mobinsheng <574923692@qq.com>
2024-06-08 14:46:26 +0800

    Update v3.py

--- v3.py
+++ v3.py
@@ -121,13 +121,13 @@ class ConvTranspose3dUpSampleBlock(nn.Module):
     pass
 
 
-class Transpose2DBlock(nn.Module):
+class Conv2D1x1Block(nn.Module):
     """
     1x1的卷积，用于改变channel数量
     """
 
     def __init__(self, input_channels, output_channels):
-        super(Transpose2DBlock, self).__init__()
+        super(Conv2D1x1Block, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(in_channels=input_channels,
                       out_channels=output_channels,
@@ -147,13 +147,13 @@ class Transpose2DBlock(nn.Module):
     pass
 
 
-class Transpose3DBlock(nn.Module):
+class Transpose3D1x1Block(nn.Module):
     """
     1x1的卷积，用于改变channel的数量
     """
 
     def __init__(self, input_channels, output_channels):
-        super(Transpose3DBlock, self).__init__()
+        super(Transpose3D1x1Block, self).__init__()
         self.model = nn.Sequential(
             nn.ConvTranspose3d(in_channels=input_channels,
                                out_channels=output_channels,
@@ -259,54 +259,16 @@ class OutputBlock(nn.Module):
     经过一个1x1的卷积层，类似于于一个全连接层
     """
 
-    def __init__(self, input_channels, outpu_channels):
+    def __init__(self, input_channels, output_channels):
         super(OutputBlock, self).__init__()
         self.model = nn.Sequential(
             nn.Conv3d(in_channels=input_channels,
-                      out_channels=8,
-                      kernel_size=1,
-                      stride=1,
-                      padding=0,
-                      bias=False),
-
-            nn.Conv3d(in_channels=8,
-                      out_channels=4,
-                      kernel_size=1,
-                      stride=1,
-                      padding=0,
-                      bias=False),
-            nn.Conv3d(in_channels=4,
-                      out_channels=2,
-                      kernel_size=1,
-                      stride=1,
-                      padding=0,
-                      bias=False),
-            nn.Conv3d(in_channels=2,
                       out_channels=1,
                       kernel_size=1,
                       stride=1,
                       padding=0,
-                      bias=False),
+                      bias=False)
         )
-        """nn.Conv3d(in_channels=4,
-                              out_channels=2,
-                              kernel_size=1,
-                              stride=1,
-                              padding=0,
-                              bias=False),
-                    nn.Conv3d(in_channels=2,
-                              out_channels=1,
-                              kernel_size=1,
-                              stride=1,
-                              padding=0,
-                              bias=False)"""
-
-        """self.conv3d = nn.Conv3d(in_channels=input_channels,
-                                out_channels=1,
-                                kernel_size=1,
-                                stride=1,
-                                padding=0,
-                                bias=False)"""
         pass
 
     def forward(self, data):
@@ -345,53 +307,55 @@ class V3(nn.Module):
     def __init__(self, input_channels, output_channels):
         super(V3, self).__init__()
 
-        self.enable_unet = False
+        self.enable_unet = True
         self.multiple = 1
 
         if self.enable_unet:
             self.multiple = 2
 
-        self.base_encoder0 = Conv2DBlock(input_channels,1024)
-        self.encoder_layer0 = Conv2DDownSampleBlock(1024, 1024)
+        self.base_conv2d0 = Conv2DBlock(input_channels,1024)
+        self.down_sample_conv2d0 = Conv2DDownSampleBlock(1024, 1024)
 
         self.link_layer0 = LinkBlock(1024, 32, 64, 64)
 
-        self.base_encoder1 = Conv2DBlock(1024, 1024)
-        self.encoder_layer1 = Conv2DDownSampleBlock(1024, 1024)
+        self.base_conv2d1 = Conv2DBlock(1024, 1024)
+        self.down_sample_conv2d1 = Conv2DDownSampleBlock(1024, 1024)
 
         self.link_layer1 = LinkBlock(1024, 64, 32, 32)
 
-        self.base_encoder2 = Conv2DBlock(1024, 1024)
-        self.encoder_layer2 = Conv2DDownSampleBlock(1024, 1024)
+        self.base_conv2d2 = Conv2DBlock(1024, 1024)
+        self.down_sample_conv2d2 = Conv2DDownSampleBlock(1024, 1024)
 
         self.link_layer2 = LinkBlock(1024, 128, 16, 16)
 
-        self.base_encoder3 = Conv2DBlock(1024, 1024)
-        self.encoder_layer3 = Conv2DDownSampleBlock(1024, 1024)
+        self.base_conv2d3 = Conv2DBlock(1024, 1024)
+        self.down_sample_conv2d3 = Conv2DDownSampleBlock(1024, 1024)
 
         self.link_layer3 = LinkBlock(1024, 256, 8, 8)
 
-        self.base_encoder4 = Conv2DBlock(1024, 1024)
-        self.encoder_layer4 = Conv2DDownSampleBlock(1024, 1024)
+        self.base_conv2d4 = Conv2DBlock(1024, 1024)
+        self.down_sample_conv2d4 = Conv2DDownSampleBlock(1024, 1024)
 
         self.link_layer4 = LinkBlock(1024, 512, 4, 4)
 
-        self.base_encoder5 = Conv2DBlock(1024, 2048)
-        self.encoder_layer5 = Conv2DDownSampleBlock(2048, 2048)
+        self.base_conv2d5 = Conv2DBlock(1024, 2048)
+        self.down_sample_conv2d5 = Conv2DDownSampleBlock(2048, 2048)
+
+        self.link_layer5 = LinkBlock(2048, 1024, 2, 2)
 
         self.transformer_layer = nn.Sequential(
-            Transpose2DBlock(2048, 2048),
+            Conv2D1x1Block(2048, 2048),
             ConvertFrom2DTo3D(),
-            Transpose3DBlock(1024, 1024),
+            Transpose3D1x1Block(1024, 1024),
 
         )
 
-        self.decoder_layer5 = ConvTranspose3dUpSampleBlock(1024 * self.multiple, 512)
-        self.decoder_layer4 = ConvTranspose3dUpSampleBlock(512 * self.multiple, 256)
-        self.decoder_layer3 = ConvTranspose3dUpSampleBlock(256 * self.multiple, 128)
-        self.decoder_layer2 = ConvTranspose3dUpSampleBlock(128 * self.multiple, 64)
-        self.decoder_layer1 = ConvTranspose3dUpSampleBlock(64 * self.multiple, 32)
-        self.decoder_layer0 = ConvTranspose3dUpSampleBlock(32 * self.multiple, 16)
+        self.up_sample_conv3d5 = ConvTranspose3dUpSampleBlock(1024 * self.multiple, 512)
+        self.up_sample_conv3d4 = ConvTranspose3dUpSampleBlock(512 * self.multiple, 256)
+        self.up_sample_conv3d3 = ConvTranspose3dUpSampleBlock(256 * self.multiple, 128)
+        self.up_sample_conv3d2 = ConvTranspose3dUpSampleBlock(128 * self.multiple, 64)
+        self.up_sample_conv3d1 = ConvTranspose3dUpSampleBlock(64 * self.multiple, 32)
+        self.up_sample_conv3d0 = ConvTranspose3dUpSampleBlock(32 * self.multiple, 16)
 
         self.output_layer = OutputBlock(16, 1)
 
@@ -399,32 +363,27 @@ class V3(nn.Module):
 
         pass
 
-    def run_normal(self, data):
-        enc0 = self.encoder_layer0(self.base_encoder0(data))
-        #link0 = self.link_layer0(enc0)
+    def run(self, data):
+        enc0 = self.down_sample_conv2d0(self.base_conv2d0(data))
 
-        enc1 = self.encoder_layer1(self.base_encoder1(enc0))
-        #link1 = self.link_layer1(enc1)
+        enc1 = self.down_sample_conv2d1(self.base_conv2d1(enc0))
 
-        enc2 = self.encoder_layer2(self.base_encoder2(enc1))
-        #link2 = self.link_layer2(enc2)
+        enc2 = self.down_sample_conv2d2(self.base_conv2d2(enc1))
 
-        enc3 = self.encoder_layer3(self.base_encoder3(enc2))
-        #link3 = self.link_layer3(enc3)
+        enc3 = self.down_sample_conv2d3(self.base_conv2d3(enc2))
 
-        enc4 = self.encoder_layer4(self.base_encoder4(enc3))
-        #link4 = self.link_layer4(enc4)
+        enc4 = self.down_sample_conv2d4(self.base_conv2d4(enc3))
 
-        enc5 = self.encoder_layer5(self.base_encoder5(enc4))
+        enc5 = self.down_sample_conv2d5(self.base_conv2d5(enc4))
 
         features = self.transformer_layer(enc5)
 
-        dec5 = self.decoder_layer5(features)
-        dec4 = self.decoder_layer4(dec5)
-        dec3 = self.decoder_layer3(dec4)
-        dec2 = self.decoder_layer2(dec3)
-        dec1 = self.decoder_layer1(dec2)
-        dec0 = self.decoder_layer0(dec1)
+        dec5 = self.up_sample_conv3d5(features)
+        dec4 = self.up_sample_conv3d4(dec5)
+        dec3 = self.up_sample_conv3d3(dec4)
+        dec2 = self.up_sample_conv3d2(dec3)
+        dec1 = self.up_sample_conv3d1(dec2)
+        dec0 = self.up_sample_conv3d0(dec1)
 
         output = self.output_layer(dec0)
 
@@ -433,28 +392,32 @@ class V3(nn.Module):
         return output
 
     def run_unet(self, data):
-        enc0 = self.encoder_layer0(self.base_encoder0(data))
+        enc0 = self.down_sample_conv2d0(self.base_conv2d0(data))
         link0 = self.link_layer0(enc0)
 
-        enc1 = self.encoder_layer1(self.base_encoder1(enc0))
+        enc1 = self.down_sample_conv2d1(self.base_conv2d1(enc0))
         link1 = self.link_layer1(enc1)
 
-        enc2 = self.encoder_layer2(self.base_encoder2(enc1))
+        enc2 = self.down_sample_conv2d2(self.base_conv2d2(enc1))
         link2 = self.link_layer2(enc2)
 
-        enc3 = self.encoder_layer3(self.base_encoder3(enc2))
+        enc3 = self.down_sample_conv2d3(self.base_conv2d3(enc2))
         link3 = self.link_layer3(enc3)
 
-        enc4 = self.encoder_layer4(self.base_encoder4(enc3))
+        enc4 = self.down_sample_conv2d4(self.base_conv2d4(enc3))
         link4 = self.link_layer4(enc4)
 
-        features = self.transformer_layer(enc4)
+        enc5 = self.down_sample_conv2d5(self.base_conv2d5(enc4))
+        link5 = self.link_layer5(enc5)
 
-        dec4 = self.decoder_layer4(torch.cat((features, link4), dim=1))
-        dec3 = self.decoder_layer3(torch.cat((dec4, link3), dim=1))
-        dec2 = self.decoder_layer2(torch.cat((dec3, link2), dim=1))
-        dec1 = self.decoder_layer1(torch.cat((dec2, link1), dim=1))
-        dec0 = self.decoder_layer0(torch.cat((dec1, link0), dim=1))
+        features = self.transformer_layer(enc5)
+
+        dec5 = self.up_sample_conv3d5(torch.cat((features, link5), dim=1))
+        dec4 = self.up_sample_conv3d4(torch.cat((dec5, link4), dim=1))
+        dec3 = self.up_sample_conv3d3(torch.cat((dec4, link3), dim=1))
+        dec2 = self.up_sample_conv3d2(torch.cat((dec3, link2), dim=1))
+        dec1 = self.up_sample_conv3d1(torch.cat((dec2, link1), dim=1))
+        dec0 = self.up_sample_conv3d0(torch.cat((dec1, link0), dim=1))
 
         output = self.output_layer(dec0)
 
@@ -467,7 +430,7 @@ class V3(nn.Module):
         if self.enable_unet:
             return self.run_unet(data)
         else:
-            return self.run_normal(data)
+            return self.run(data)
 
 
 if __name__ == "__main__":
